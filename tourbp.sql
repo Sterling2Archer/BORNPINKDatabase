f024768b7365b173aa9a2fd4c14eab313b7bd949
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-09-21 18:43:32 -0400

    Update tourbp.sql

--- tourbp.sql
+++ tourbp.sql
@@ -11,4 +11,3 @@ INSERT INTO tour.fans VALUES (1, 'Canada', 'November 6th', 'Hamilton', '17300');
 INSERT INTO tour.fans VALUES (2, 'USA', 'November 10th', 'Chicago', '23500');
 INSERT INTO tour.fans VALUES (3, 'USA', 'November 14th', 'Newark', '19500');
 INSERT INTO tour.fans VALUES (4, 'USA', 'November 19th', 'Los Angeles', '22000');
-
