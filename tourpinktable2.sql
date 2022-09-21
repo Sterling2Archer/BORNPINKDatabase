458e4db0feaf4541b1e258ce184047eba6929bde
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-09-21 18:43:22 -0400

    Update tourpinktable2.sql

--- tourpinktable2.sql
+++ tourpinktable2.sql
@@ -7,9 +7,7 @@ Resell_Tickets char(40) NULL,
 Total_Remaining_Tickets char(19) NOT NULL
 );
 
-INSERT INTO tour.orders VALUES ('1', 'Hamilton', '66', '1,000', '1,066');
-INSERT INTO tour.orders VALUES ('2', 'Chicago', '56', '2,000', '2,056');
-INSERT INTO tour.orders VALUES ('3', 'Newark', '61', '3,000', '3,061');
-INSERT INTO tour.orders VALUES ('4', 'Los Angeles', '150', '6,000', '6,150');
-
-
+INSERT INTO tour.orders VALUES ('1', 'Hamilton', '66', '1000', '1066');
+INSERT INTO tour.orders VALUES ('2', 'Chicago', '56', '2000', '2056');
+INSERT INTO tour.orders VALUES ('3', 'Newark', '61', '3000', '3061');
+INSERT INTO tour.orders VALUES ('4', 'Los Angeles', '150', '6000', '6150');
