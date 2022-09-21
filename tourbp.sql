8b2c8d79b959766e9711d6a32f760314fab4e8e1
Evania Torres <106711685+Sterling2Archer@users.noreply.github.com>
2022-09-21 18:42:55 -0400

    Update tourbp.sql

--- tourbp.sql
+++ tourbp.sql
@@ -7,8 +7,8 @@ City char(40) NULL,
 Venue_Capacity char(19) NOT NULL
 );
 
-INSERT INTO tour.fans VALUES (1, 'Canada', 'November 6th', 'Hamilton', '17,300');
-INSERT INTO tour.fans VALUES (2, 'USA', 'November 10th', 'Chicago', '23,500');
-INSERT INTO tour.fans VALUES (3, 'USA', 'November 14th', 'Newark', '19,500');
-INSERT INTO tour.fans VALUES (4, 'USA', 'November 19th', 'Los Angeles', '22,000');
+INSERT INTO tour.fans VALUES (1, 'Canada', 'November 6th', 'Hamilton', '17300');
+INSERT INTO tour.fans VALUES (2, 'USA', 'November 10th', 'Chicago', '23500');
+INSERT INTO tour.fans VALUES (3, 'USA', 'November 14th', 'Newark', '19500');
+INSERT INTO tour.fans VALUES (4, 'USA', 'November 19th', 'Los Angeles', '22000');
 
